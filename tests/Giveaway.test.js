2dfb31415c03b632bc80b461afafe208662722ff
Ryan Kruberg <rkruberg@gmail.com>
2021-05-05 17:37:05 +0300

    Removed describe block in jest since it's inferred by the file name

--- tests/Giveaway.test.js
+++ tests/Giveaway.test.js
@@ -6,22 +6,20 @@ const {
   WebScraping
 } = require('../src/WebScraping');
 
-describe('Testing giveaway module', () => {
-  test('Testing if giveaways have been found', (done) => {
-    function checkGiveaway(data) {
-      try {
-        let giveaways = WebScraping.GetSteamAnnouncements(data);
-        expect(giveaways.length).not.toBe(0);
-        done();
-      } catch (error) {
-        done(error);
-      }
+test('Testing if giveaways have been found', (done) => {
+  function checkGiveaway(data) {
+    try {
+      let giveaways = WebScraping.GetSteamAnnouncements(data);
+      expect(giveaways.length).not.toBe(0);
+      done();
+    } catch (error) {
+      done(error);
     }
+  }
 
-    Object.keys(Giveaways.URL).forEach((key) => {
-      let url = Giveaways.URL[key];
-      // Return SimpleFetch<Promise> did not work
-      WebScraping.SimpleFetch(url).then(checkGiveaway);
-    });
+  Object.keys(Giveaways.URL).forEach((key) => {
+    let url = Giveaways.URL[key];
+    // Return SimpleFetch<Promise> did not work
+    WebScraping.SimpleFetch(url).then(checkGiveaway);
   });
 });
