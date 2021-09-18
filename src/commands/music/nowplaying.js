e68b7745bb755488978db43fa3485894a038220f
Ryan Kruberg <rkruberg@gmail.com>
2021-09-18 14:16:36 +0300

    copied music bot commands from discord-bot by TannerGabriel

---
+++ src/commands/music/nowplaying.js
@@ -0,0 +1,54 @@
+import { GuildMember } from 'discord.js';
+
+export default {
+  name: 'nowplaying',
+  description: 'Get the song that is currently playing.',
+  async execute(interaction, player) {
+    if (
+      !(interaction.member instanceof GuildMember) ||
+      !interaction.member.voice.channel
+    ) {
+      return interaction.reply({
+        content: 'You are not in a voice channel!',
+        ephemeral: true,
+      });
+    }
+
+    if (
+      interaction.guild.me.voice.channelId &&
+      interaction.member.voice.channelId !==
+        interaction.guild.me.voice.channelId
+    ) {
+      return interaction.reply({
+        content: 'You are not in my voice channel!',
+        ephemeral: true,
+      });
+    }
+
+    await interaction.deferReply();
+    const queue = player.getQueue(interaction.guildId);
+    if (!queue || !queue.playing)
+      return interaction.followUp({
+        content: '❌ | No music is being played!',
+      });
+    const progress = queue.createProgressBar();
+    const perc = queue.getPlayerTimestamp();
+
+    return interaction.followUp({
+      embeds: [
+        {
+          title: 'Now Playing',
+          description: `🎶 | **${queue.current.title}**! (\`${perc.progress}%\`)`,
+          fields: [
+            {
+              name: '\u200B',
+              value: progress,
+            },
+          ],
+          // eslint-disable-next-line prettier/prettier
+          color: 0xFF_FF_FF,
+        },
+      ],
+    });
+  },
+};
