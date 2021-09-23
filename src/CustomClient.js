e85ea601980611650a8e39d327cb668d2b459333
Ryan Kruberg <rkruberg@gmail.com>
2021-09-23 18:25:13 +0300

    refactored DiscordBot client

---
+++ src/CustomClient.js
@@ -0,0 +1,47 @@
+import { Player } from 'discord-player';
+import { Client, Collection } from 'discord.js';
+import Logging from './Logging.js';
+
+export default class DiscordBot extends Client {
+  constructor(options) {
+    super(options);
+    this.commands = new Collection();
+    this.handlers = new Collection();
+
+    this.player = new Player(this);
+    this.player.on('error', (queue, error) => {
+      Logging.Log(
+        `[${queue.guild.name}] Error emitted from the queue: ${error.message}`
+      );
+    });
+    this.player.on('connectionError', (queue, error) => {
+      Logging.Log(
+        `[${queue.guild.name}] Error emitted from the connection: ${error.message}`
+      );
+    });
+    this.player.on('botDisconnect', (queue) => {
+      queue.metadata.send(
+        '❌ | I was manually disconnected from the voice channel, clearing queue!'
+      );
+    });
+
+    this.player.on('trackAdd', (queue, track) => {
+      queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
+    });
+    this.player.on('tracksAdd', (queue, tracks) => {
+      queue.metadata.send(`🎶 | ${tracks.length} tracks added to the queue!`);
+    });
+    this.player.on('trackStart', (queue, track) => {
+      queue.metadata.send(
+        `🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`
+      );
+    });
+
+    this.player.on('channelEmpty', (queue) => {
+      queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
+    });
+    this.player.on('queueEnd', (queue) => {
+      queue.metadata.send('✅ | Queue finished!');
+    });
+  }
+}
--- src/Identification.js
+++ src/Identification.js
@@ -1,45 +1,6 @@
-import { Player } from 'discord-player';
-import { Client, User, Guild, Intents, Collection } from 'discord.js';
+import { Client, User, Guild, Intents } from 'discord.js';
+import DiscordBot from './CustomClient.js';
 import Logging from './Logging.js';
-
-export class DiscordBot extends Client {
-  constructor(options) {
-    super(options);
-    this.commands = new Collection();
-    this.handlers = new Collection();
-
-    this.player = new Player(this);
-    this.player.on('error', (queue, error) => {
-      Logging.Log(
-        `[${queue.guild.name}] Error emitted from the queue: ${error.message}`
-      );
-    });
-    this.player.on('connectionError', (queue, error) => {
-      Logging.Log(
-        `[${queue.guild.name}] Error emitted from the connection: ${error.message}`
-      );
-    });
-    this.player.on('trackStart', (queue, track) => {
-      queue.metadata.send(
-        `🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`
-      );
-    });
-    this.player.on('trackAdd', (queue, track) => {
-      queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
-    });
-    this.player.on('botDisconnect', (queue) => {
-      queue.metadata.send(
-        '❌ | I was manually disconnected from the voice channel, clearing queue!'
-      );
-    });
-    this.player.on('channelEmpty', (queue) => {
-      queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
-    });
-    this.player.on('queueEnd', (queue) => {
-      queue.metadata.send('✅ | Queue finished!');
-    });
-  }
-}
 
 export const client = new DiscordBot({
   intents: [
