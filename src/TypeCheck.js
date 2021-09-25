08c759271c133567f0fa2e2d3d723f3a6e172c61
Ryan Kruberg <rkruberg@gmail.com>
2021-09-26 00:12:22 +0300

    made GetMsgEmbed take into account a mixture of strings and and EmbedFields doesn't check if a field is strictly an EmbedFields however - assumes that

--- src/TypeCheck.js
+++ src/TypeCheck.js
@@ -1,18 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
 export function filterInt(value) {
   if (/^[+-]?(\d+|Infinity)$/.test(value)) {
     return Number(value);
   }
   return Number.NaN;
-}
-
-/** Checks if input is an array of strings or strings
- * @param {String | String[] | import('discord.js').EmbedField |
- *  import('discord.js').EmbedField[]} toCheck
- * @returns true if 'toCheck' is an array of string or string
- */
-export function CheckStringArray(toCheck) {
-  const bool =
-    (Array.isArray(toCheck) && typeof toCheck[0] === 'string') ||
-    typeof toCheck === 'string';
-  return bool;
 }
