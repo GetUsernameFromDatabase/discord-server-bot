37a90a06a435aa5512b3632654616129398a21c7
Ryan Kruberg <rkruberg@gmail.com>
2021-09-29 11:18:36 +0300

    refactored Player events; options in initializing player do not work for me

--- src/CustomClient.js
+++ src/CustomClient.js
@@ -2,41 +2,50 @@ import { Player } from 'discord-player';
 import { Client, Collection } from 'discord.js';
 import Logging from './Logging.js';
 
+/**
+ * @param {Player} player */
+function addEventsToPlayer(player) {
+  player.on('error', (queue, error) => {
+    Logging.Log(
+      `[${queue.guild.name}] Error emitted from the queue: ${error.message}`
+    );
+  });
+  player.on('connectionError', (queue, error) => {
+    Logging.Log(
+      `[${queue.guild.name}] Error emitted from the connection: ${error.message}`
+    );
+  });
+  player.on('botDisconnect', (queue) => {
+    queue.metadata.send(
+      '❌ | I was manually disconnected from the voice channel, clearing queue!'
+    );
+  });
+
+  player.on('trackAdd', (queue, track) => {
+    queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
+  });
+  player.on('tracksAdd', (queue, tracks) => {
+    queue.metadata.send(`🎶 | ${tracks.length} tracks added to the queue!`);
+  });
+
+  player.on('channelEmpty', (queue) => {
+    queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
+  });
+  player.on('queueEnd', (queue) => {
+    queue.metadata.send('✅ | Queue finished!');
+  });
+}
+
 export default class DiscordBot extends Client {
   constructor(options) {
     super(options);
     this.commands = new Collection();
     this.handlers = new Collection();
 
-    this.player = new Player(this, { leaveOnEmptyCooldown: 30 * 1000 });
-    this.player.on('error', (queue, error) => {
-      Logging.Log(
-        `[${queue.guild.name}] Error emitted from the queue: ${error.message}`
-      );
+    this.player = new Player(this, {
+      leaveOnEnd: false,
+      leaveOnEmptyCooldown: 30_000,
     });
-    this.player.on('connectionError', (queue, error) => {
-      Logging.Log(
-        `[${queue.guild.name}] Error emitted from the connection: ${error.message}`
-      );
-    });
-    this.player.on('botDisconnect', (queue) => {
-      queue.metadata.send(
-        '❌ | I was manually disconnected from the voice channel, clearing queue!'
-      );
-    });
-
-    this.player.on('trackAdd', (queue, track) => {
-      queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
-    });
-    this.player.on('tracksAdd', (queue, tracks) => {
-      queue.metadata.send(`🎶 | ${tracks.length} tracks added to the queue!`);
-    });
-
-    this.player.on('channelEmpty', (queue) => {
-      queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
-    });
-    this.player.on('queueEnd', (queue) => {
-      queue.metadata.send('✅ | Queue finished!');
-    });
+    addEventsToPlayer(this.player);
   }
 }
