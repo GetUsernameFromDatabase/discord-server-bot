b30ca906cdd7cdae477383eabc1d079ee204b3ba
Ryan Kruberg <rkruberg@gmail.com>
2021-10-16 02:41:42 +0300

    improved mock

--- __mocks__/discord.js.js
+++ __mocks__/discord.js.js
@@ -27,7 +27,7 @@ const botMock = {
 export class Client extends Discord.Client {
   constructor(options) {
     super(options);
-    this.user = botMock;
+    this.user = new Discord.User(this, { ...botMock });
   }
 }
 export class User extends Discord.User {}
