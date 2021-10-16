2c82f810f75648f498a2ae42eab17f11d1eee748
Ryan Kruberg <rkruberg@gmail.com>
2021-10-17 00:06:08 +0300

    babel changes

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,36 +1,32 @@
 // eslint-disable-next-line import/unambiguous
-const {
-  engines: { node },
-} = require('./package.json');
-
 module.exports = (api) => {
   const isTest = api.env('test');
   const baseObject = {
     plugins: [
       ['@babel/plugin-proposal-class-properties'],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          absoluteRuntime: false,
-          corejs: '3',
-          version: '^7.14.6',
-        },
-      ],
+      // [
+      //   '@babel/plugin-transform-runtime',
+      //   {
+      //     absoluteRuntime: false,
+      //     corejs: '3',
+      //     version: '^7.14.6',
+      //   },
+      // ],
     ],
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            esmodules: true,
-            node: node.slice(2),
-          },
-          modules: false,
-          useBuiltIns: 'usage',
-          corejs: { version: '3.14', proposals: true },
-        },
-      ],
-    ],
+    // presets: [
+    //   [
+    //     '@babel/preset-env',
+    //     {
+    //       targets: {
+    //         esmodules: true,
+    //         node: 'current',
+    //       },
+    //       modules: false,
+    //       useBuiltIns: 'usage',
+    //       corejs: { version: '3.14', proposals: true },
+    //     },
+    //   ],
+    // ],
   };
 
   if (isTest)
