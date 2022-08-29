1b6178fc15fc4a8ad9b4382a2754eb9a7452368f
Ryan Kruberg <rkruberg@gmail.com>
2022-08-29 21:54:04 +0300

    dev: reddit

---
+++ __tests__/Reddit.test.js
@@ -0,0 +1,27 @@
+import RedditFetch from '../src/services/Reddit.js';
+
+test('hot', async () => {
+  expect.assertions(1);
+  const reddit = await RedditFetch({
+    subreddit: 'Memes_Of_The_Dank',
+    sort: 'hot',
+  });
+
+  console.error(reddit);
+  expect(reddit).toBeDefined();
+});
+
+// test('new', () => {
+//   expect.assertions(1);
+//   const reddit = new Reddit('Memes_Of_The_Dank', 'new');
+// });
+
+// test('top', () => {
+//   expect.assertions(1);
+//   const reddit = new Reddit('Memes_Of_The_Dank', 'top');
+// });
+
+// test('rising', () => {
+//   expect.assertions(1);
+//   const reddit = new Reddit('Memes_Of_The_Dank', 'rising');
+// });
